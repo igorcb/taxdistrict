e14bd35f568b5717a2429d30e8a3a5dca7fec8cb
Igor C. Batista <igor.batista@gmail.com>
2016-04-02 13:15:40 -0300

    when edit rate, modifi price and if not exist rate invert register rate inverter

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -7,4 +7,18 @@ class Rate < ActiveRecord::Base
   validates :price, presence: true
   
 	validates_uniqueness_of :district_origin_id, :scope => [:district_target_id]
+
+	after_save :modifi_price_invert_rate
+
+  def modifi_price_invert_rate
+    #procura pela rota invertida
+    rate_invert = Rate.find_by_district_origin_id_and_district_target_id(self.district_target_id, self.district_origin_id)
+    #cadastra ou atualiza o preco da rota invertida a rota
+    if rate_invert.nil?
+    	Rate.create!(district_origin_id: self.district_target_id, district_target_id: self.district_origin_id, price: price) 
+    else
+      Rate.where(id: rate_invert.id).update_all(price: self.price)    	
+    end
+  end
+
 end
